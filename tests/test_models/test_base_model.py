071a511296f89639a9c1fdb60c69de5bb30c050e
marwenrebah <6863@holbertonstudents.com>
2023-11-27 00:47:48 -1100

    tests

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,16 @@
+#usr/bin/python3
+"""unitetst"""
+
+import unittest
+from models.basemodel import BaseModel
+
+
+class testBaseModel(unittest.TestCase):
+    """This class is for testing"""
+    
+    
+
+
+
+    if __name__ == "__main__":
+    unittest.main()
